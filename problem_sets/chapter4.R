8b1232fe6e8198558d211ba3666dab61e1693da8
madelinemberger <maddiemt8@gmail.com>
2026-02-18 16:23:53 -1000

    save lecture edits to chapter 4 r doc

--- problem_sets/chapter4.R
+++ problem_sets/chapter4.R
@@ -14,6 +14,7 @@ library(bayesplot)
 library(dplyr)
 library(ggplot2)
 library(tidybayes)
+library(tidyverse)
 
 # data --------------------------------------------------------------------
 
@@ -88,6 +89,10 @@ model {
 }
 
 generated quantities {
+  vector[N] y_hat;
+  for(i in 1:N){
+  y_hat[i] = normal_rng(alpha + beta * x[i], sigma); // simulated data, this is where we account for sigma
+  }
 } 
 ",
 
@@ -112,7 +117,47 @@ mcmc_trace(fit.m4.3, pars=c("alpha","beta","sigma"))
 # plot results
 fit.m4.3.sum <- fit.m4.3 %>% 
   spread_draws(alpha,beta) %>%  #extract posterior samples for intercept and slope into a tibble
-  median_qi() # summarize
+  median_qi() # summarize, get the upper lower and median of each parameter
 ypred <- fit.m4.3.sum$alpha + fit.m4.3.sum$beta*d2$weight2
 plot(height ~ d2$weight2, data=d2 , col=rangi2, xlab="weight")
 points(d2$weight2,ypred,type="l")
+
+
+# look at lines
+ggplot()+
+  geom_point(data=d2, aes(x=weight2, y=height))+
+  geom_abline(
+    data = post %>% sample_n(10),
+    aes(intercept = alpha,
+        slope = beta),
+    alpha = 0.5, color = "skyblue"
+  )
+
+# predict mean at a certain place, what is the average height if x is a certain weight?
+
+mu_at_50 <- post$alpha + post$beta*(50 - mean(d2$weight)) # 1500 estimates of height at X
+plot(density(mu_at_50))
+
+# Or, could predict every mu
+# first create a sequence of x you are interested in, which will be on the horizontal axis
+
+# for every value of x, create a sequence. now we have 1500 x 100 = 150000 rows 
+# for each new value of x, what are the posterior lines you could draw with the 1500 alphas and betas?
+x_grid <- d2 %>% 
+  expand(weight2 = seq(min(weight2), max(weight2), length.out = 100))
+
+draws_with_preds <- fit.m4.3 %>% 
+  spread_draws(alpha, beta) %>% 
+  cross_join(x_grid) %>%
+  mutate(mu_hat = alpha + beta * weight2) # plugging in each x to each alpha and beta to get a y hat
+
+# we made a mini posterior for each value x, 1500 mu hats
+ggplot(draws_with_preds,
+       aes(x = weight2, y = y_hat, group = .draw))+
+  geom_point(alpha = 0.5, color = "coral")
+
+# but where is sigma? its a generated quantity! stan does it for you as you sample the mcmc
+# we need sigma to estimate y hat
+
+
+  
